25cef8e5b7590f6f82f141e7c331eb96f692acb6
GeorgePlukov <gplukov@gmail.com>
2016-12-12 15:11:16 -0500

    Error message and new wcomment styling

--- public/js/controllers/threadCtrl.js
+++ public/js/controllers/threadCtrl.js
@@ -4,24 +4,46 @@ angular.module('threadCtrl', [])
         // set title L
         this.title = "AJDOASOUDASUODASOUDH!";
         $scope.comments = [{
-                "username": "plukovga",
-                "comment": "Do you think he got into that job excited about hundreds of strangers probing his rectum, or did he just shrug his shoulders and figure \"eh it\'s a job, beats retail anyways\"Haven't decided which one is funnier to me",
-                score: 1136,
-                timesince: "1 day",
-                hidden: false
-            }, {
-                "username": "usertwo.name",
-                "comment": "Do you think he got into that job excited about hundreds of strangers probing his rectum, or did he just shrug his shoulders and figure \"eh it\'s a job, beats retail anyways\"Haven't decided which one is funnier to me",
-                score: 745,
-                timesince: "22 hours",
-                hidden: true
-            }];
-
-        $scope.newcomment = {"username":"","comment": ""};
-            // Set voting variable
+            "username": "plukovga",
+            "comment": "Do you think he got into that job excited about hundreds of strangers probing his rectum, or did he just shrug his shoulders and figure \"eh it\'s a job, beats retail anyways\"Haven't decided which one is funnier to me",
+            score: 1136,
+            timesince: "1 day",
+            hidden: false
+        }, {
+            "username": "usertwo.name",
+            "comment": "Do you think he got into that job excited about hundreds of strangers probing his rectum, or did he just shrug his shoulders and figure \"eh it\'s a job, beats retail anyways\"Haven't decided which one is funnier to me",
+            score: 745,
+            timesince: "22 hours",
+            hidden: false
+        }, {
+            "username": "usertwo.name",
+            "comment": "Do you think he got into that job excited about hundreds of strangers probing his rectum, or did he just shrug his shoulders and figure \"eh it\'s a job, beats retail anyways\"Haven't decided which one is funnier to me",
+            score: 745,
+            timesince: "22 hours",
+            hidden: false
+        }, {
+            "username": "usertwo.name",
+            "comment": "Do you think he got into that job excited about hundreds of strangers probing his rectum, or did he just shrug his shoulders and figure \"eh it\'s a job, beats retail anyways\"Haven't decided which one is funnier to me",
+            score: 745,
+            timesince: "22 hours",
+            hidden: false
+        }, {
+            "username": "usertwo.name",
+            "comment": "Do you think he got into that job excited about hundreds of strangers probing his rectum, or did he just shrug his shoulders and figure \"eh it\'s a job, beats retail anyways\"Haven't decided which one is funnier to me",
+            score: 745,
+            timesince: "22 hours",
+            hidden: false
+        }];
+        // Define new comment items
+        $scope.commentError = "";
+        $scope.newcomment = {
+            "username": "",
+            "comment": ""
+        };
+        // Set voting variable
         self.userVoted = false;
         self.score = 1226;
-        // $scope.show = true;
+        $scope.show = true;
         $("#viewThreadModal").modal('show');
 
         $scope.upvote = function() {
@@ -48,8 +70,15 @@ angular.module('threadCtrl', [])
 
         // Minimize the comments
         $scope.minimize = function(index) {
-          $scope.show = !$scope.show;
-          $scope.comments[index].hidden = !$scope.comments[index].hidden;
+            $scope.show = !$scope.show;
+            $scope.comments[index].hidden = !$scope.comments[index].hidden;
 
+        };
+        $scope.submit = function() {
+            if ($scope.newcomment.comment !== "") {
+              
+            } else {
+                $scope.commentError = "Comment cannot be left empty";
+            }
         };
     });
