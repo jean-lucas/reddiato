cf7cafe2a5039fad5691e81b197d46a8847d679b
jean-lucas <jeanlpf@hotmail.com>
2016-12-12 15:15:34 -0500

    lo;

--- public/js/controllers/userPageCtrl.js
+++ public/js/controllers/userPageCtrl.js
@@ -5,5 +5,10 @@ angular.module('userPageCtrl', [])
 	.controller('userPageCtrl',  function($scope){
 		var self = this;
 
-		this.test = "a";
+		this.userName = "username";
+
+		this.toggleVal;
+
+		
+		
 	})
