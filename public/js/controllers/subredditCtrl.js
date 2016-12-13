4dcb77386565d24f32f4e8d09186ca5d1416e8a9
jean-lucas <jeanlpf@hotmail.com>
2016-12-12 19:27:19 -0500

    fixing user page

--- public/js/controllers/subredditCtrl.js
+++ public/js/controllers/subredditCtrl.js
@@ -14,6 +14,9 @@ angular.module('subredditCtrl', [])
 		    $scope.switchContent(self.allUsers[self.currentUserId].username);
 		}
 
+
+
+
 		$scope.openThread = function(){
 			$('#modal').modal('show');
 		};
