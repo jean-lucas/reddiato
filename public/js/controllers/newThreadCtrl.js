114682de705d91701e5c298b7667fa17e010167b
GeorgePlukov <gplukov@gmail.com>
2016-12-13 12:51:30 -0500

    added syling for switch on new thread

--- public/js/controllers/newThreadCtrl.js
+++ public/js/controllers/newThreadCtrl.js
@@ -1,5 +1,7 @@
 angular.module('newThreadCtrl', [])
     .controller('newThreadController', function($scope) {
         var self = this;
+        this.toggleVal;
+        $("#newThreadModal").modal('show');
 
     });
